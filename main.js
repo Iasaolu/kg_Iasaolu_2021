7f965343c96987c45f9281c4897de066d1a41e47
Iasaolu <54831309+Iasaolu@users.noreply.github.com>
2021-05-01 10:12:13 -0400

    Created a function to convert numbers to words

---
+++ main.js
@@ -0,0 +1,20 @@
+function digitsToWords(arr){
+    let answer = "";
+    let words=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+    for (let i=0; i<arr.length; i++){
+        if (arr[i].toString().length === 1){
+            answer+=words[arr[i]];
+            answer+=(",");
+        }
+
+        else if(arr[i].toString().length > 1){
+            let inside = arr[i].toString().split("");
+            for( let k = 0; k<inside.length; k++){
+                answer+=words[inside[k]];
+            }
+            answer+=",";
+        }
+    }
+    let fixed_answer =answer.substring(0, answer.length-1);
+    console.log(fixed_answer);
+}
